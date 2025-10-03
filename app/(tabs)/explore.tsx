a05d27569a8b0f2408ecacf5f102eabd80d71612
Elbadii <37973058-Elbadii@users.noreply.replit.com>
2025-10-03 18:27:04 +0000

    Restored to '3b51cb445af4c6252cabcb5e743ef8d2ace05a5c'
    Replit-Restored-To: 3b51cb445af4c6252cabcb5e743ef8d2ace05a5c

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,14 +1,25 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, ScrollView } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
 
 export default function TabTwoScreen() {
   return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.content}>
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={
+        <IconSymbol
+          size={310}
+          color="#808080"
+          name="chevron.left.forwardslash.chevron.right"
+          style={styles.headerImage}
+        />
+      }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Explore</ThemedText>
       </ThemedView>
@@ -81,16 +92,16 @@ export default function TabTwoScreen() {
           ),
         })}
       </Collapsible>
-    </ScrollView>
+    </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  content: {
-    padding: 20,
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
   },
   titleContainer: {
     flexDirection: 'row',
